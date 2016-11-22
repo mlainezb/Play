4ae76f07a0cf283b90b682c34cd310aaa379cc05
manuel <mlainezb@gmail.com>
2016-11-21 22:11:24 -0300

    hice hasta donde me dio el tiempo

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,17 @@ class Users::RegistrationsController < Devise::RegistrationsController
 # before_action :configure_sign_up_params, only: [:create]
 # before_action :configure_account_update_params, only: [:update]
 
+
+    before_action  :configure_sign_up_params ,  only : [ :create ]   
+
+    protected
+
+
+    # If you have extra params to permit, append them to the sanitizer. 
+    def  configure_sign_up_params     
+      devise_parameter_sanitizer . permit ( :sign_up ,  keys : [ :name ] ) 
+      end 
+     end
   # GET /resource/sign_up
   # def new
   #   super
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+    enum role: [:trainer, :admin]
+  	before_create :set_role
 
-    has_many :user_song
+
+    has_many  :user_song,  dependent : :destroy
 	has_many :songs, through: :user_song
 
   devise :database_authenticatable, :registerable,
